a17eac2c7bff21471b794950e21d6172550ea08d
Yuji Tsuchimoto <yuji@eikai.co.jp>
2014-08-09 05:28:30 +0900

    the first implement for swing

--- server/realtime/nodejs/api01/swingapi/app.js
+++ server/realtime/nodejs/api01/swingapi/app.js
@@ -34,3 +34,46 @@ app.get('/', routes.index);
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
+
+
+// ================================================================
+/**
+ * Module independencies.
+ */
+
+// event loop
+var io = require('socket.io').listen(app);
+// common environment
+var totalSwingMagnitude = 0;
+// connection loop
+io.sockets.on('connection', function (socket) {
+
+  // trigger by API commands
+  socket.on('send msg', function (msg) {
+    // echo back 
+    socket.emit('push msg', msg);
+    // broadcast
+    socket.broadcast.emit('push msg', msg);
+  });
+  socket.on('send swing', function (mag) {
+	  totalSwingMagnitude += parseInt(mag);
+    var val = Math.round(totalSwingMagnitude);
+    // echo back 
+    socket.emit('push swing', val );
+  });
+  socket.on('get total swing', function () {
+    var val = Math.round(totalSwingMagnitude);
+    // echo back 
+    socket.emit('push swing', val );
+  });
+
+  // trigger by disconnection
+  socket.on('disconnect', function() {
+    console.log('disconnected');
+  });
+});
+
+// execute intervally
+setInterval( function () {
+  totalSwingMagnitude *= 0.5;
+}, 1000 );
--- server/realtime/nodejs/api01/swingapi/public/javascripts/api.js
+++ server/realtime/nodejs/api01/swingapi/public/javascripts/api.js
@@ -1,24 +1,38 @@
 $(function() {
-  var socket = io.connect('http://m-ph.org:3000');
+  var socket = io.connect('http://api.m-ph.org:3000');
   socket.on('connect', function() {
     console.log('connected');
   });
 
   $('#btn').click(function() {
     var message = $('#message');
-    console.log(message);
+    // console.log(message);
     // push msg command to server
     socket.emit('send msg', message.val());
   });
   // trigger for receiving msg from server
   socket.on('push msg', function (msg) {
-    console.log(msg);
-    var date = new Date();
-    $('#list').prepend($('<dt>' + date + '</dt><dd>' + msg + '</dd>'));
+    //console.log(msg);
+    $('#list').prepend($('<dt>message</dt><dd>' + msg + '</dd>'));
   });
-  /*
+
+  $('#swing').click(function() {
+    var magnitude = $('#magnitude');
+    // push magnitude command to server
+    socket.emit('send swing', magnitude.val());
+  });
+  // trigger for receiving msg from server
+  socket.on('push swing', function (mag) {
+    //console.log(mag);
+    $('#ss').html('<dt>盛り上がり度</dt><dd>' + mag + '</dd>');
+  });
+
   socket.on('msg updateDB', function(msg){
     console.log(msg);
   });
-  */
+
+  setInterval( function() {
+    socket.emit('get total swing');
+  }, 1000 );
 });
+
