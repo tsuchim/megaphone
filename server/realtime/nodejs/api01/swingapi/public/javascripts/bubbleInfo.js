f2e6851acb2d2d75086894a4d4248d966fce5231
Tatsuo TOMITA <owl.tommy@gmail.com>
2014-09-26 17:06:31 +0900

    バブルのデザインを正円に変更

--- server/realtime/nodejs/api01/swingapi/public/javascripts/bubbleInfo.js
+++ server/realtime/nodejs/api01/swingapi/public/javascripts/bubbleInfo.js
@@ -1,7 +1,12 @@
-function BubbleInfo(id, color, magnitude ){
+function BubbleInfo(id, magnitude, color ){
     this.id = id;
     this.color = color;
     this.magnitude = magnitude;
-    this.margin;
 
+    this.margin = Math.floor( Math.random() * 100 );
+
+    this.size = function(){
+	return 10 * Math.sqrt( this.magnitude );
+    }
 }
+
--- server/realtime/nodejs/api01/swingapi/public/javascripts/bubbleManager.js
+++ server/realtime/nodejs/api01/swingapi/public/javascripts/bubbleManager.js
@@ -5,8 +5,8 @@ $.fn.bubbleManager = function (options) {
     var current_disploy_number = 0;
 
     var addBubble = function(){
+
 	var bubbleInfo = new BubbleInfo( $(options.idForm).val(), $(options.magForm).val(), $(options.colorForm).val() );
-        bubbleInfo.margin = Math.floor( Math.random() * 100 );
 
 	var bubble = createBubble( bubbleInfo );
 	self.append(bubble);
@@ -39,10 +39,17 @@ $.fn.bubbleManager = function (options) {
     var createBubble = function( info ) {
         var bubble = $("<div/>");
         bubble.addClass( "bubble" );
-        bubble.html( "o" );
+        bubble.html( );
         bubble.width( "1em" );
         bubble.css("position", "absolute" );
 	bubble.attr("id", info.id );
+
+	var size = info.size();;
+	bubble.css( "width", size );
+	bubble.css( "height", size );
+	bubble.css( "-webkit-border-radius", (size/2) );
+	bubble.css( "-moz-border-radius", (size/2) );
+	bubble.css( "border-radius", (size/2) );
 
         bubble.css( { 'margin-left' : info.margin +'%' } );
         return bubble;
