97d57ee031de00362cc08c74f3bc6b1a14d6fc9a
Yuji Tsuchimoto <yuji@eikai.co.jp>
2014-10-08 20:52:08 +0900

    gh-pages の余分なファイルを削除

--- server/realtime/nodejs/api01/swingapi/public/javascripts/bubble.js
+++
@@ -1,8 +0,0 @@
-function BubbleInfo(id, color, magnitude ){
-    this.id = id;
-    this.color = color;
-    this.magnitude = magnitude;
-    this.margin;
-
-    alert( id + " : " + color + " : " + magnitude );
-}
